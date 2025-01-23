4be8960ed06cecff51fd3e19490f12f63ace0ac6
athuljain <athuljainkj2@gmail.com>
2025-01-23 09:13:24 +0530

    ifelse

---
+++ JS24/Indroduction/2js.js
@@ -0,0 +1,39 @@
+// let age = 16;
+
+// if (age >= 18) {
+//     console.log("You are eligible to vote.");
+// } else {
+//     console.log("You are not eligible to vote.");
+// }
+
+
+// --------------------------------------------------------------------
+
+let marks = 85;
+
+if (marks >= 90) {
+    console.log("Grade: A");
+} else if (marks >= 75) {
+    console.log("Grade: B");
+} else if (marks >= 50) {
+    console.log("Grade: C");
+} else {
+    console.log("Grade: F");
+}
+
+
+// -----------------------------------------------------------------------
+
+// Practice Example:
+// Write a program to determine if a person is a child, teenager, or adult based on their age.
+
+
+let age = 15;
+
+if (age < 13) {
+    console.log("You are a child.");
+} else if (age >= 13 && age <= 19) {
+    console.log("You are a teenager.");
+} else {
+    console.log("You are an adult.");
+}
