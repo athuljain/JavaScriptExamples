8e0d5a8447f7501264fc25529d0c4433874d6358
athuljain <athuljainkj2@gmail.com>
2025-01-23 08:46:14 +0530

    js operators

---
+++ JS24/Indroduction/1theory.js
@@ -0,0 +1,109 @@
+// // Using var
+// var greeting = "Hello";
+// console.log(greeting); // Output: Hello
+
+// // Using let
+// let age = 25;
+// console.log(age); // Output: 25
+
+// age = 26; // Updating value
+// console.log(age); // Output: 26
+
+// // Using const
+// const country = "India";
+// console.log(country); // Output: India
+
+// // country = "USA"; // Error: Assignment to constant variable
+
+
+// ------------------------------------------------------------------------------------
+
+
+// var x = 10;
+// var y = 20;
+// var z = x + y; // z is 30
+
+// var str1 = "hello";
+// var str2 = "world";
+// var str3 = str1 + " " + str2; // str3 is "hello world"
+
+
+// console.log(str3);
+
+
+
+// ----------------------------------------------------------------------
+
+
+// JavaScript Operators and Examples
+
+
+
+// Arithmetic Operators
+
+//  + (Addition), - (Subtraction), * (Multiplication), / (Division), % (Modulus).
+
+// let a = 10;
+// let b = 3;
+
+// console.log(a + b); // Output: 13 (Addition)
+// console.log(a - b); // Output: 7 (Subtraction)
+// console.log(a * b); // Output: 30 (Multiplication)
+// console.log(a / b); // Output: 3.333... (Division)
+// console.log(a % b); // Output: 1 (Modulus - Remainder)
+
+
+
+// Comparison Operators
+// Used to compare two values. The result is a boolean (true or false).
+
+// Examples: >, <, >=, <=, ==, !=, ===, !==.
+
+// let x = 5;
+// let y = 10;
+
+// console.log(x > y); // Output: false (Greater than)
+// console.log(x < y); // Output: true (Less than)
+// console.log(x == y); // Output: false (Equality)
+// console.log(x != y); // Output: true (Not equal)
+// console.log(x === 5); // Output: true (Strict equality - same value & type)
+// console.log(x !== "5"); // Output: true (Strict inequality)
+
+
+
+// Logical Operators
+// Used to combine or invert boolean values.
+
+// Examples: && (AND), || (OR), ! (NOT).
+
+
+// let p = true;
+// let q = false;
+
+// console.log(p && q); // Output: false (AND - both must be true)
+// console.log(p || q); // Output: true (OR - at least one must be true)
+// console.log(!p);     // Output: false (NOT - inverts the value)
+
+
+
+// Assignment Operators
+// Used to assign values to variables.
+
+// Examples: =, +=, -=, *=, /=.
+
+// let num = 10;
+
+// num += 5; // num = num + 5
+// console.log(num); // Output: 15
+
+// num *= 2; // num = num * 2
+// console.log(num); // Output: 30
+
+
+// Conditional (Ternary) Operator
+// A shorthand for if-else statements. Syntax: condition ? valueIfTrue : valueIfFalse.
+
+let age = 18;
+
+let message = (age >= 18) ? "You are an adult" : "You are a minor";
+console.log(message); // Output: "You are an adult"
