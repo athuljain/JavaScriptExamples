9d7874015c4153f621eb0fab96805441b905f4c6
athuljain <athuljainkj2@gmail.com>
2025-01-24 08:24:28 +0530

    practical q

--- JS24/Indroduction/3jsloop.js
+++ JS24/Indroduction/3jsloop.js
@@ -16,3 +16,23 @@
 // }
 // // Output: 1, 2, 3, 4, 5
 
+
+
+// do while loop
+
+// let number = 1;
+
+// do {
+//     console.log("The current number is: " + number);
+//     number++;
+// } while (number <= 10);
+
+// // Output:
+// // The current number is: 1
+// // The current number is: 2
+// // The current number is: 3
+// // ...
+// // The current number is: 10
+
+
+
