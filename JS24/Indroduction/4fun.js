4eb266c41b3741014ff9fa78398e70e9a63ebc21
athuljain <athuljainkj2@gmail.com>
2025-01-28 08:41:27 +0530

    functions almost

--- JS24/Indroduction/4fun.js
+++ JS24/Indroduction/4fun.js
@@ -30,9 +30,51 @@
 // Immediately Invoked Function Expressions (IIFE): 
 
 
-        (function() {
-            console.log("This function runs immediately!");
-        })();
+        // (function() {
+        //     console.log("This function runs immediately!");
+        // })();
 
+
+
+
+//  Print numbers from 1 to 10 using a while loop.? (named function)
+
+                function printNumbers() {
+                    let i = 1;
+                    while (i <= 10) {
+                        console.log(i);
+                        i++;
+                    }
+                }
+                printNumbers();
 
         
+
+//  Calculate the sum of all odd numbers from 1 to 50 using a while loop.?Anonymous Function)
+
+
+        // const calculateOddSum = function() {
+        //     let sum = 0;
+        //     let i = 1;
+        //     while (i <= 50) {
+        //         if (i % 2 !== 0) {
+        //             sum += i;
+        //         }
+        //         i++;
+        //     }
+        //     console.log("Sum of odd numbers from 1 to 50:", sum);
+        // };
+        // calculateOddSum();
+
+
+
+// Halve a number repeatedly until it becomes less than 1, printing each result.? (Arrow Function)
+
+
+        // const halveNumber = (num) => {
+        //     while (num >= 1) {
+        //         console.log(num);
+        //         num /= 2;
+        //     }
+        // };
+        // halveNumber(100);
