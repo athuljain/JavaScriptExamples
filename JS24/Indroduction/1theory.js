e979089d0b41e9fa4c43f4514077de79833b36e3
athuljain <athuljainkj2@gmail.com>
2025-03-19 08:42:05 +0530

    js base theory

--- JS24/Indroduction/1theory.js
+++ JS24/Indroduction/1theory.js
@@ -1,109 +1,109 @@
-// // Using var
-// var greeting = "Hello";
-// console.log(greeting); // Output: Hello
+// Using var
+var greeting = "Hello";
+console.log(greeting); 
 
-// // Using let
-// let age = 25;
-// console.log(age); // Output: 25
+// Using let
+let age = 25;
+console.log(age); 
 
-// age = 26; // Updating value
-// console.log(age); // Output: 26
+age = 26; // Updating value
+console.log(age); 
 
-// // Using const
-// const country = "India";
-// console.log(country); // Output: India
+// Using const
+const country = "India";
+console.log(country); 
 
-// // country = "USA"; // Error: Assignment to constant variable
-
-
-// ------------------------------------------------------------------------------------
+// country = "USA"; // Error: Assignment to constant variable
 
 
-// var x = 10;
-// var y = 20;
-// var z = x + y; // z is 30
-
-// var str1 = "hello";
-// var str2 = "world";
-// var str3 = str1 + " " + str2; // str3 is "hello world"
+------------------------------------------------------------------------------------
 
 
-// console.log(str3);
+var x = 10;
+var y = 20;
+var z = x + y; 
+
+var str1 = "hello";
+var str2 = "world";
+var str3 = str1 + " " + str2; 
 
 
-
-// ----------------------------------------------------------------------
-
-
-// JavaScript Operators and Examples
+console.log(str3);
 
 
 
-// Arithmetic Operators
+----------------------------------------------------------------------
 
-//  + (Addition), - (Subtraction), * (Multiplication), / (Division), % (Modulus).
 
-// let a = 10;
-// let b = 3;
-
-// console.log(a + b); // Output: 13 (Addition)
-// console.log(a - b); // Output: 7 (Subtraction)
-// console.log(a * b); // Output: 30 (Multiplication)
-// console.log(a / b); // Output: 3.333... (Division)
-// console.log(a % b); // Output: 1 (Modulus - Remainder)
+JavaScript Operators and Examples
 
 
 
-// Comparison Operators
-// Used to compare two values. The result is a boolean (true or false).
+Arithmetic Operators
 
-// Examples: >, <, >=, <=, ==, !=, ===, !==.
+ + (Addition), - (Subtraction), * (Multiplication), / (Division), % (Modulus).
 
-// let x = 5;
-// let y = 10;
+let a = 10;
+let b = 3;
 
-// console.log(x > y); // Output: false (Greater than)
-// console.log(x < y); // Output: true (Less than)
-// console.log(x == y); // Output: false (Equality)
-// console.log(x != y); // Output: true (Not equal)
-// console.log(x === 5); // Output: true (Strict equality - same value & type)
-// console.log(x !== "5"); // Output: true (Strict inequality)
+console.log(a + b); 
+console.log(a - b); 
+console.log(a * b); 
+console.log(a / b); 
+console.log(a % b); 
 
 
 
-// Logical Operators
-// Used to combine or invert boolean values.
+Comparison Operators
+Used to compare two values. The result is a boolean (true or false).
 
-// Examples: && (AND), || (OR), ! (NOT).
+Examples: >, <, >=, <=, ==, !=, ===, !==.
 
+let x = 5;
+let y = 10;
 
-// let p = true;
-// let q = false;
-
-// console.log(p && q); // Output: false (AND - both must be true)
-// console.log(p || q); // Output: true (OR - at least one must be true)
-// console.log(!p);     // Output: false (NOT - inverts the value)
-
-
-
-// Assignment Operators
-// Used to assign values to variables.
-
-// Examples: =, +=, -=, *=, /=.
-
-// let num = 10;
-
-// num += 5; // num = num + 5
-// console.log(num); // Output: 15
-
-// num *= 2; // num = num * 2
-// console.log(num); // Output: 30
+console.log(x > y); 
+console.log(x < y); 
+console.log(x == y); 
+console.log(x != y); 
+console.log(x === 5);
+console.log(x !== "5"); 
 
 
-// Conditional (Ternary) Operator
-// A shorthand for if-else statements. Syntax: condition ? valueIfTrue : valueIfFalse.
 
-// let age = 18;
+Logical Operators
+Used to combine or invert boolean values.
 
-// let message = (age >= 18) ? "You are an adult" : "You are a minor";
-// console.log(message); // Output: "You are an adult"
+Examples: && (AND), || (OR), ! (NOT).
+
+
+let p = true;
+let q = false;
+
+console.log(p && q);
+console.log(p || q); 
+console.log(!p);     
+
+
+
+Assignment Operators
+Used to assign values to variables.
+
+Examples: =, +=, -=, *=, /=.
+
+let num = 10;
+
+num += 5; 
+console.log(num);
+
+num *= 2;
+console.log(num);
+
+
+Conditional (Ternary) Operator
+A shorthand for if-else statements. Syntax: condition ? valueIfTrue : valueIfFalse.
+
+let age = 18;
+
+let message = (age >= 18) ? "You are an adult" : "You are a minor";
+console.log(message); 
