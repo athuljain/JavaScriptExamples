3f7c59bf12d05927e3b509f1c8437c5aa2da5cb7
athuljain <athuljainkj2@gmail.com>
2025-06-23 10:51:30 +0530

    js updates 6

---
+++ JS24/Indroduction/jsone.js
@@ -0,0 +1,33 @@
+const student={
+    name:"shifana",
+    age:21,
+    email:"shifanaibrahim68@gmail.com",
+
+};
+console.log(student.name)
+
+
+let num=8
+if(num%2===0){
+    console.log("the number is even")
+    
+}else{
+    console.log("the number is odd")
+}
+
+let number=80
+if(number>90){
+    console.log("Grade A");
+
+}else if(number>=75){
+    console.log("Grade B");
+
+}
+else if(number>=50){
+    console.log("Grade c");
+}
+else {
+    console.log("fail");
+}
+    
+    
