eb41d31d115b0c025a320dd744af066f3947fcaa
athuljain <athuljainkj2@gmail.com>
2025-07-25 12:01:18 +0530

    js updates 10.10

--- JS24/Indroduction/jsone.js
+++ JS24/Indroduction/jsone.js
@@ -1,44 +1,44 @@
-const student={
-    name:"shifana",
-    age:21,
-    email:"shifanaibrahim68@gmail.com",
+// const student={
+//     name:"shifana",
+//     age:21,
+//     email:"shifanaibrahim68@gmail.com",
 
-};
-console.log(student.name)
+// };
+// console.log(student.name)
 
 
-let num=8
-if(num%2===0){
-    console.log("the number is even")
+// let num=8
+// if(num%2===0){
+//     console.log("the number is even")
     
-}else{
-    console.log("the number is odd")
-}
+// }else{
+//     console.log("the number is odd")
+// }
 
-let number=80
-if(number>90){
-    console.log("Grade A");
+// let number=80
+// if(number>90){
+//     console.log("Grade A");
 
-}else if(number>=75){
-    console.log("Grade B");
+// }else if(number>=75){
+//     console.log("Grade B");
 
-}
-else if(number>=50){
-    console.log("Grade c");
-}
-else {
-    console.log("fail");
-}
+// }
+// else if(number>=50){
+//     console.log("Grade c");
+// }
+// else {
+//     console.log("fail");
+// }
     
     
 
 
-const add=()=> console.log(5+3)
-add()
+// const add=()=> console.log(5+3)
+// add()
 
 
 
-function sum(name){
-    return `hi, ${name}`;
-}
-console.log(sum("sdfghjkl"))
+// function sum(name){
+//     return `hi, ${name}`;
+// }
+// console.log(sum("sdfghjkl"))
