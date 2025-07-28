ed46e9a04aba101c3daca36238dd9b9ee1966695
athuljain <athuljainkj2@gmail.com>
2025-07-28 09:29:53 +0530

    js updates 10.11

---
+++ JS24/Indroduction/6String.js
@@ -0,0 +1,60 @@
+// How do you reverse a string in JavaScript?
+// Answer:
+// Use .split(), .reverse(), and .join() methods.
+
+
+// const str = "hello";
+// const reversed = str.split("").reverse().join("");
+// console.log(reversed); 
+
+
+// ----------------------------------------------------------------------
+
+// How can you check if a string contains a specific word?
+// Answer:
+// Use .includes() method.
+
+// const message = "Welcome to JavaScript!";
+// console.log(message.includes("JavaScript"));
+// console.log(message.includes("Python"));     
+
+
+
+
+// -----------------------------------------------------
+
+// How to replace a word in a string with another word?
+// Answer:
+// Use .replace() method.
+
+
+
+// const text = "I love PHP";
+// const newText = text.replace("PHP", "JavaScript");
+// console.log(newText); 
+
+
+// ------------------------------------------------------------------
+
+
+
+// How do you extract a part of a string?
+// Answer:
+// Use .substring(start, end) or .slice(start, end).
+
+
+// const str = "JavaScript Developer";
+// console.log(str.substring(0, 10)); 
+// console.log(str.slice(0, 10));     
+
+// ---------------------------------------------------------------
+
+
+// How can you convert a string to uppercase and lowercase?
+// Answer:
+// Use .toUpperCase() and .toLowerCase() methods.
+
+
+// const name = "athul";
+// console.log(name.toUpperCase()); // "ATHUL"
+// console.log(name.toLowerCase()); // "athul"
